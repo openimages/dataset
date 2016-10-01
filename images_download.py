d03616d9332508e05445d9622a6a3bfc316b8519
Naveen <Naveenal@users.noreply.github.com>
2016-09-30 22:32:09 -0700

    First commit
    Python script to download all the images from the images.csv file and map each file name with the labels.

---
+++ images_download.py
@@ -0,0 +1,29 @@
+''' 
+  Download all the images from the images.csv file
+'''
+
+import csv
+import sys
+import urllib
+import unicodecsv as csv
+
+i = 0
+r_file = open('images.csv', 'rt')
+w_file = open('img_label.csv', 'wt')
+writer = csv.writer(w_file, encoding='utf-8')
+
+images = csv.reader(r_file)
+img_label = csv.writer(w_file)
+
+try:    
+    imgs = iter(images)
+    next(imgs)
+    for img in imgs:
+        i += 1
+        urllib.urlretrieve (img[1], 'image_%d.jpg' % i)
+        img_id = ('image_%d.jpg .... Saved' % i)
+        print (img_id)
+        img_label.writerow(['image_%d.jpg' % i , img[6]])
+finally:
+    r_file.close()
+    w_file.close()
